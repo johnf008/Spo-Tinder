7c562341758e49a5af978b05e71702cc2b257b86
johnf008 <Carlos.flores.h108@gmail.com>
2025-06-25 20:44:58 -0500

    Was able to fix the card, but working on buttons now

--- src/components/Main_Card.jsx
+++ src/components/Main_Card.jsx
@@ -3,10 +3,16 @@ function Main_Card() {
 
     return (
         <>
-        <div className="block border-red-500 place-items-center mt-5 text-center m-auto w-100 h-125 bg-zinc-900 rounded-xl text-white font-bold">
+        <div className="block border-red-500 place-items-center mt-5 text-center m-auto w-100 h-125 bg-zinc-900 rounded-xl text-white ">
             <img className="block border border-red-500 m-0 p-0" src="https://placehold.co/250"></img>
-            <p className="block border border-red-500 m-0 p-0 leading-none">Song name</p>
             
+                <p className="text-left text-3xl border border-red-500 mt-5 leading-none w-80 font-bold">Song name</p>
+                <p className="text-left text-2xl border border-red-500 mt-5 leading-none w-80">Artist</p>
+        </div>
+
+        <div className="flex justify g">
+            <button className="bg-green-200 mt-5 text-center m-auto w-40 h-15 rounded-xl cursor-pointer hover:bg-green-300">Add To Playlist</button>
+             <button className="bg-green-200 mt-5 text-center m-auto w-40 h-15 rounded-xl cursor-pointer hover:bg-green-300">Add To Playlist</button>
         </div>
         </>
     )
